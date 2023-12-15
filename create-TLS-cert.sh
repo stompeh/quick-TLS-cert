5d23536dd0388bd1f78bd74ac5e065f729df0cf2
stompeh <107382256+stompeh@users.noreply.github.com>
2023-12-15 15:02:44 -0500

    Update create-TLS-cert.sh

--- create-TLS-cert.sh
+++ create-TLS-cert.sh
@@ -13,9 +13,13 @@ echo "Generating cert-key\n"
 openssl genrsa -out cert-key.pem 4096
 openssl req -new -sha256 -subj "/CN=SomeName" -key cert-key.pem -out cert.csr
 
-# Use a hostname or IP. Needs to be declared
+# Use a hostname or IP.
 # DNS:<hostname>
 # IP:<ipaddress>
+# Can use multiple lines:
+# echo "subjectAltName=DNS:doop.myserver.com" >> extfile.cnf
+# echo "subjectAltName=DNS:files.myserver.com" >> extfile.cnf
+# echo "subjectAltName=IP:10.10.15.15" >> extfile.cnf
 echo "subjectAltName=DNS:localhost" >> extfile.cnf
 
 openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf -CAcreateserial
