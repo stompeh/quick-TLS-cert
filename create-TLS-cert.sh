483bb8b47973088626d130a43faa1c453dbdc48b
stompeh <107382256+stompeh@users.noreply.github.com>
2023-12-15 14:00:31 -0500

    Create create-TLS-cert.sh

---
+++ create-TLS-cert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir certs
+cd certs
+
+echo "Generating AES ca-key\n"
+# To use a password:
+# openssl genrsa -aes256 -out ca-key.pem 4096
+openssl genrsa -out ca-key.pem 4096
+openssl req -new -x509 -sha256 -days 365 -key ca-key.pem -out ca.pem
+
+echo "Generating cert-key\n"
+openssl genrsa -out cert-key.pem 4096
+openssl req -new -sha256 -subj "/CN=Tropical Foods" -key cert-key.pem -out cert.csr
+
+echo "subjectAltName=IP:10.8.0.175" >> extfile.cnf
+
+openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf -CAcreateserial
+
+
+#On Debian
+#Move the CA certificate (ca.pem) into /usr/local/share/ca-certificates/ca.crt.
+#Update the Cert Store with:
+#sudo update-ca-certificates
