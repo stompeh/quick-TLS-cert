f70c9f95f228578a10e90342e6c83f0bf1ba12ad
stompeh <107382256+stompeh@users.noreply.github.com>
2023-12-15 14:03:52 -0500

    Update create-TLS-cert.sh

--- create-TLS-cert.sh
+++ create-TLS-cert.sh
@@ -13,6 +13,9 @@ echo "Generating cert-key\n"
 openssl genrsa -out cert-key.pem 4096
 openssl req -new -sha256 -subj "/CN=Tropical Foods" -key cert-key.pem -out cert.csr
 
+# Use a hostname or IP. Needs to be declared
+# DNS:<hostname>
+# IP:<ipaddress>
 echo "subjectAltName=IP:10.8.0.175" >> extfile.cnf
 
 openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf -CAcreateserial
